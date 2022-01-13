a2eb41e2674718435d2b6b6407e7361d19bb7387
Le Xuan Hung <43202901+HungLe051902@users.noreply.github.com>
2022-01-13 17:29:34 +0700

    Add

--- WebApp/src/services/BaseAxios.js
+++ WebApp/src/services/BaseAxios.js
@@ -1,14 +1,14 @@
 import axios from "axios";
+import { getToken } from "@/helpers/authenticationHelper.js";
 
 export const HTTP = axios.create({
   baseURL: `https://localhost:44303/api`,
-  // headers: {
-  //   Authorization: "Bearer {token}",
-  //   "Access-Control-Allow-Origin": "*",
-  //   "Access-Control-Allow-Methods": "HEAD, GET, POST, PUT, PATCH, DELETE",
-  //   "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
-  // },
   headers: {
     "Access-Control-Allow-Origin": "*",
   },
+});
+
+axios.interceptors.request.use((request) => {
+  request.headers.common.Authorization = `Bearer ${getToken()}`;
+  return request;
 });
